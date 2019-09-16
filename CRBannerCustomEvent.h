f7fab7d1f7edf23d15832f3aa383ea9ee599c3e9
Julien Stoeffler <j.stoeffler@criteo.com>
2019-09-16 16:15:42 -0700

    Fix mopub.h import

--- CRBannerCustomEvent.h
+++ CRBannerCustomEvent.h
@@ -17,7 +17,7 @@
 // limitations under the License.
 
 #import <Foundation/Foundation.h>
-#import <MoPub.h>
+#import "MoPub.h"
 #import "CRCriteoAdapterConfiguration.h"
 @import CriteoPublisherSdk;
 
